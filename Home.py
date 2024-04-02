18b8fc3f81742bf9eb02e2093df9504a52b7ea6a
ashwinsingh2007 <legend.ashwini07@gmail.com>
2024-04-02 16:09:38 +0530

    .. first phase chatgpt extended

--- Home.py
+++ Home.py
@@ -15,5 +15,4 @@ st.write("""
 - **Chatbot with Internet Access**: An internet-enabled chatbot capable of answering user queries about recent events.
 - **Chat with your PDF** Empower the chatbot with the ability to access custom documents, enabling it to provide answers to user queries based on the referenced information.
 
-To explore sample usage of each chatbot, please navigate to the corresponding chatbot section.
 """)
